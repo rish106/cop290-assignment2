07e14acaa350519d2efb537b59b67b5703ea90e6
Rishabh Verma <10rishabhv@gmail.com>
2023-02-19 02:54:58 +0530

    minor changes to hashmap

--- part2/include/hm.h
+++ part2/include/hm.h
@@ -27,7 +27,7 @@ unsigned int hash(const char *key) {
 int hashmap_create(struct hashmap_s *const out_hashmap) {
     for (int i = 0; i < SZ; i++) {
         out_hashmap->table[i] = list_new();
-        // out_hashmap->lk[i] = lock_new();
+        out_hashmap->lk[i] = lock_new();
     }
     return 1;
 }
@@ -37,15 +37,13 @@ int hashmap_put(struct hashmap_s *const hashmap, const char* key, void* data) {
     struct hashmap_element_s* new_item = (struct hashmap_element_s*)malloc(sizeof(struct hashmap_element_s));
     new_item->key = (char*)key;
     new_item->data = data;
-    if (!is_empty(hashmap->table[hash(key)])) {
-        struct listentry* e = hashmap->table[hash(key)]->head;
-        while (e != NULL) {
-            if (!strcmp(((struct hashmap_element_s*)(e->data))->key, key)) {
-                ((struct hashmap_element_s*)(e->data))->data = data;
-                return 1;
-            } else {
-                e = e->next;
-            }
+    struct listentry* e = hashmap->table[hash(key)]->head;
+    while (e != NULL) {
+        if (!strcmp(((struct hashmap_element_s*)(e->data))->key, key)) {
+            ((struct hashmap_element_s*)(e->data))->data = data;
+            return 1;
+        } else {
+            e = e->next;
         }
     }
     list_add(hashmap->table[hash(key)], new_item);
@@ -55,10 +53,14 @@ int hashmap_put(struct hashmap_s *const hashmap, const char* key, void* data) {
 // Fetch value of a key from hashmap
 void* hashmap_get(struct hashmap_s *const hashmap, const char* key) {
     struct listentry* e = hashmap->table[hash(key)]->head;
-    while (strcmp(((struct hashmap_element_s*)(e->data))->key, (char*)key)) {
+    while (e != NULL && strcmp(((struct hashmap_element_s*)(e->data))->key, key)) {
         e = e->next;
     }
-    return ((struct hashmap_element_s*)(e->data))->data;
+    if (e == NULL) {
+        return NULL;
+    } else {
+        return ((struct hashmap_element_s*)(e->data))->data;
+    }
 }
 
 // Execute argument function on each key-value pair in hashmap
@@ -66,11 +68,19 @@ void hashmap_iterator(struct hashmap_s* const hashmap, int (*f)(struct hashmap_e
     for (int i = 0; i < SZ; i++) {
         struct listentry* e = hashmap->table[i]->head;
         while (e != NULL) {
-            f((struct hashmap_element_s* const)e->data);
+            f((struct hashmap_element_s* const)(e->data));
             e = e->next;
         }
     }
 }
 
-int acquire_bucket(struct hashmap_s *const hashmap, const char* key);   // Acquire lock on a hashmap slot
-int release_bucket(struct hashmap_s *const hashmap, const char* key);   // Release acquired lock
+// Acquire lock on a hashmap slot
+int acquire_bucket(struct hashmap_s *const hashmap, const char* key) {
+    lock_acquire((struct lock*)(hashmap->lk[hash(key)]));
+    return 1;
+}
+
+// Release acquired lock
+int release_bucket(struct hashmap_s *const hashmap, const char* key) {
+    return lock_release((struct lock*)(hashmap->lk[hash(key)]));
+}
